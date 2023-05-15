5f73f5edf37d7922b9e2359c322abe3a309e0542
mukhlish <mukhlish99@gmail.com>
2023-05-15 23:37:25 +0800

    add login

--- lib/views/widgets/info_dialog.dart
+++ lib/views/widgets/info_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 InfoDialog() {
   final player = AudioPlayer();
@@ -11,7 +12,7 @@ InfoDialog() {
         children: [
           Container(
             alignment: Alignment.center,
-            height: 230,
+            height: 240,
             width: 300,
             child: ListView(
               children: [
@@ -37,12 +38,41 @@ InfoDialog() {
               ],
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                onPressed: () {
+                  _launchURL_apiLogin();
+                },
+                label: Text(
+                  "Login",
+                  style: TextStyle(color: Color.fromARGB(255, 12, 75, 127)),
+                ),
+                icon: Icon(Icons.login),
+                style: ButtonStyle(
+                    iconColor: MaterialStateProperty.all(
+                        Color.fromARGB(255, 12, 75, 127))),
+              )
+            ],
+          ),
         ],
       ),
       backgroundColor: Color.fromARGB(255, 218, 165, 6),
       middleTextStyle: TextStyle(color: Color.fromARGB(255, 90, 3, 3)),
-      titlePadding: EdgeInsets.all(10),
+      titlePadding: EdgeInsets.all(0),
       contentPadding: EdgeInsets.all(0),
       radius: 70);
   // Future.delayed(Duration(seconds: 1), () => Get.back());
+}
+
+_launchURL_apiLogin() async {
+  // print("test");
+  const url = 'https://wed-api.mirulazam.store/wish/index';
+  final uri = Uri.parse(url);
+  if (await canLaunchUrl(uri))
+    return await launchUrl(uri);
+  else
+    // can't launch url, there is some error
+    return throw "Could not launch $uri";
 }
